4f0d1291f60d9edd91b6d1438c78f2d6854feded
Kevin Botero <kbotero201@gmail.com>
2020-09-09 18:26:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
-  new_array = []
-  counter = 0
 
-while counter < 5 array.each do |integer|
-   new_array << integer ** 2
-  end
-  return new_array
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end

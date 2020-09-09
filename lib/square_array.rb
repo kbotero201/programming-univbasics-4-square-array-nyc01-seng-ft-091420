a1be9d34a689c5ba38c317c1ef010312334ed770
Kevin Botero <kbotero201@gmail.com>
2020-09-09 18:41:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,4 @@
-#def square_array(numbers)
-  count = 0
-  while count < numbers.length
-    numbers.index ** 2
-    count += 1
-  end
-  return square_array(numbers)
-end
 
 def square_array(numbers)
     numbers.map { |e| e ** 2 }
-end 
+end
